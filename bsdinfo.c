27e09c9fbd38670b0c4277142d68ef2265509b97
Jakub Szafrański <s@samu.pl>
2012-04-06 11:52:01 +0200

    Strip spaces from hw.model sysctl, file cleanup

--- bsdinfo.c
+++ bsdinfo.c
@@ -40,104 +40,7 @@
 #include <string.h>
 #include <sys/user.h>
 
-#define O_RDONLY 0x0000
-#define _POSIX2_LINE_MAX 2048
-#define _PATH_DEVNULL "/dev/null"
-
-int psysctl(char *name)
-{
-    char buf[256];
-    size_t size = sizeof(buf);
-    
-    if (sysctlbyname(name, &buf, &size, NULL, 0) != 0) {
-        return (1);
-    }
-    printf("%s", buf);
-    return (0);
-}
-
-int printval(char *val, char *name) 
-{
-    printf("\033[1;31m%s:\033[0;0m ", val);
-    psysctl(name);
-}
-
-int printuptime() 
-{
-    struct timespec tp;
-    time_t uptime;
-    int days, hrs, i, mins, secs;
-    char buf[256];
-    
-    if (clock_gettime(CLOCK_UPTIME, &tp) != -1) {
-        uptime = tp.tv_sec;
-        if (uptime > 60)
-            uptime += 30;
-        days = uptime / 86400;
-        uptime %= 86400;
-        hrs = uptime / 3600;
-        uptime %= 3600;
-        mins = uptime / 60;
-        secs = uptime % 60;
-        (void)printf("\033[1;31mUptime: \033[0;0m");
-        if (days > 0)
-            (void)printf("%d day%s ", days, days > 1 ? "s" : "");
-        if (hrs > 0 && mins > 0)
-            (void)printf("%d:%02d", hrs, mins);
-        else if (hrs == 0 && mins > 0) 
-            (void)printf("00:%02d", hrs, mins);
-        else 
-            (void)printf("00:00");
-    }
-}
-
-int printshell()
-{
-    char* shell = getenv("SHELL");
-    if (shell != NULL) {
-        int slen = strlen(shell);
-        for (slen; slen > 0; --slen) if (shell[slen] == '/') break;
-        slen = slen+1;
-        while(shell[slen] != '\0') {
-            printf("%c", shell[slen]);
-            ++slen;
-        }
-        if (slen == 1) printf("%s", shell);
-    }
-    return (0);
-}
-
-int printmem()
-{
-    printf("\033[1;31mRAM:\033[0;0m ");
-
-    long int ram_free = 0;
-    size_t size_ram_free;
-    size_ram_free = sizeof(ram_free);
-    sysctlbyname("vm.stats.vm.v_free_count", &ram_free, &size_ram_free, NULL, 0);
-
-    long int ram_pagesize = 0;
-    size_t size_ram_pagesize;
-    size_ram_pagesize = sizeof(ram_pagesize);
-    sysctlbyname("hw.pagesize", &ram_pagesize, &size_ram_pagesize, NULL, 0);
-    ram_free = ram_free*ram_pagesize;
-    ram_free = ram_free/1024/1024;
-    
-        
-    long int ram_size = 0;
-    size_t size_ram_size;
-    size_ram_size = sizeof(ram_size);
-    sysctlbyname("hw.physmem", &ram_size, &size_ram_size, NULL, 0);
-    ram_size = ram_size/1024/1024;
-
-    ram_free = ram_size - ram_free;
-    
-    printf("%dM / ", ram_free);
-    printf("%dM", ram_size);
-    
-    return (0);
-    
-}
+#include "bsdinfo.h"
 
 int main()
 {
@@ -165,7 +68,7 @@ int main()
     printf("\033[1;31m   y/               -/`   -o/\033[0;0m        "); printuptime(); printf("\n");
     printf("\033[1;31m  .-                  ::/sy+:.\033[0;0m       "); printf("\033[1;31mProcesses:\033[0;0m %d\n", proc_count);
     printf("\033[1;31m  /                     `--  /\033[0;0m       "); printmem(); printf("\n");
-    printf("\033[1;31m `:                          :`\033[0;0m      "); printval("CPU", "hw.model");  printf("\n");
+    printf("\033[1;31m `:                          :`\033[0;0m      "); printcpu();  printf("\n");
     printf("\033[1;31m `:                          :`\033[0;0m      "); printf("\033[1;31mShell:\033[0;0m ", proc_count); printshell(); printf("\n");
     printf("\033[1;31m  /                          /\033[0;0m       \n");
     printf("\033[1;31m  .-                        -.\033[0;0m       \n");
